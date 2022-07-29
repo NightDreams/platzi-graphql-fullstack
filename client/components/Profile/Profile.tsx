7bd90ac64e8886ece250923fe3c11d0c4c77998f
NightDreams <yayodcv@gmail.com>
2022-07-28 20:18:53 -0500

    c11  Setup Auth , Apollo Client -  createHttpLink, setContext, retrieveToken

--- client/components/Profile/Profile.tsx
+++ client/components/Profile/Profile.tsx
@@ -1,11 +1,56 @@
 import { Segment, Header, Button } from 'semantic-ui-react'
-
 import { removeToken } from '@service/auth'
 import type { User } from '@service/auth'
 
 import Layout from '@components/Layout/Layout'
+/* 
+  1 import document 
+  2 imp - request framework > tratar con el document 
+  3 setup mutation 
+  4 setup  event to run  mutation
+  5 -setup auth > client 
+ */
+import { AddAvocadoDocument } from '@service/graphql'
+import { useMutation } from '@apollo/client'
 
 function Profile({ user }: { user: User }) {
+  /* 
+  useMutation
+   trigiger , {data , loading  }
+  -- 
+  addAvocado 
+  --
+  */
+
+  const [addAvocado, { data, loading }] = useMutation(AddAvocadoDocument)
+  console.log(
+    '🚀 ~ file: Profile.tsx ~ line 20 ~ Profile ~ data, loading',
+    data,
+    loading
+  )
+
+  // 4 mutate props
+  /* arg -  variables:{} - receibe values with gon to run mutation  
+  variables - las mismas que espera el schema, inside "data"
+como nuestro query in "queries.graphql"
+  */
+  const createAvo = async () => {
+    addAvocado({
+      variables: {
+        data: {
+          name: 'Zutano Avocado 2 ',
+          sku: 'MW79ZZEWV6Y',
+          price: 1.42,
+          image: 'static/zutano.jpg',
+          description:
+            'The zutano avocado is a cold hardy, consistent producing avocado variety. ',
+          shape: 'Pear',
+          hardiness: '-5ºC',
+          taste: 'Splendid, is an avocado',
+        },
+      },
+    })
+  }
   const logout = async () => {
     await removeToken()
     window.location.reload()
@@ -21,7 +66,7 @@ function Profile({ user }: { user: User }) {
         <p>
           Si estás viendo esto es porque has iniciado sesión de forma correcta.
         </p>
-        <Button type="button" positive>
+        <Button type="button" positive onclick={createAvo}>
           Agregar nuevo avocado...
         </Button>{' '}
         <Button type="button" basic color="red" onClick={logout}>
--- client/service/client.ts
+++ client/service/client.ts
@@ -1,8 +1,60 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { retrieveToken } from '@service/auth'
+import { symbolName } from 'typescript'
 import { baseUrl } from './config'
 
-const client = new ApolloClient({
+/* now > incluir headers
+  1 createHttpLink - crear un const 
+  - envoler la url . 
+  - + 
+  2 setContext >
+  - run cualquier funcion que queramos
+  - async, puede serlo.
+  - 
+  -  control auth. Ejecutable de forma dinamica.
+--- 
+  1 arg "_" no nos importa 
+  2 nuevo contexto a darle a  apollo. 
+   - headers > determinar auth 
+  
+  Apollo maneja lso enlaces a los servidores con  "link"
+--- 
+3 Setup auth 
+ - aut > set header Bearer token
+ - !auth > null - sin cambios
+ - server/ cliente (next)- comportamietno.
+--- 
+ a - retrieveToken -  get token from sesion . saber si esta autenticado
+ b - run only en client> comprobanod - window existe 
+ El codigo corre en server y cliente 
+*/
+const apiLink = createHttpLink({
   uri: `${baseUrl}/graphql`,
+})
+
+const authLInk = setContext(async (_, { headers }) => {
+  let extraHeaders = {}
+
+  if (typeof window !== 'undefined') {
+    // run only in client
+    const token = await retrieveToken()
+    extraHeaders = {
+      Authorization: `Bearer ${token}`,
+    }
+  }
+
+  return {
+    headers: {
+      ...headers,
+      ...extraHeaders,
+    },
+  }
+})
+
+const client = new ApolloClient({
+  // cambiamos "uri" por un lin. 2 links juntos. auth + http link
+  link: authLInk.concat(apiLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
